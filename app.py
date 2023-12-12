e7f65a7f53fcb23c8f3d35aef6481d65453961c6
Kamil Stachurski <kamil.stachurski@icloud.com>
2023-12-12 09:09:20 +0100

    edit app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import tensorflow as tf
+
+def preprocess_image(data, shape = 224, scale = False):
+    img = tf.io.read_file(data)
+    img = tf.io.decode_image(img, channels = 3)
+    img = tf.image.resize(img, [shape, shape])
+    if scale:
+        return img / 255.
+    else:
+        return img
 
 st.title('👁️ Food-Vision 🍔')
 st.header('Identify what\'s on your food image!')
