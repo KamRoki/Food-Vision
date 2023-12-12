1e220b120322dfa577b92d8040453091c4fbb8a8
Kamil Stachurski <kamil.stachurski@icloud.com>
2023-12-12 09:06:29 +0100

    edit app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ st.header('Identify what\'s on your food image!')
 st.write('To know more about this app, visit [**GitHub**](https://github.com/KamRoki/Food-Vision)')
 image_data = st.file_uploader(label = 'Upload your food image.',
                         type = ['jpg', 'jpeg', 'png'])
-st.image(image_data)
+#st.image(image_data)
