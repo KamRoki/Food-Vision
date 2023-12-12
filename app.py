a737f1858c0ae31fe88052b419462111d2a63c81
Kamil Stachurski <kamil.stachurski@icloud.com>
2023-12-12 09:05:55 +0100

    edit app.py

--- app.py
+++ app.py
@@ -3,5 +3,6 @@ import streamlit as st
 st.title('👁️ Food-Vision 🍔')
 st.header('Identify what\'s on your food image!')
 st.write('To know more about this app, visit [**GitHub**](https://github.com/KamRoki/Food-Vision)')
-file = st.file_uploader(label = 'Upload your food image.',
+image_data = st.file_uploader(label = 'Upload your food image.',
                         type = ['jpg', 'jpeg', 'png'])
+st.image(image_data)
